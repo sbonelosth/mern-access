dca4cc154ec43a45f562e8dd86c02e0ad8bc88e6
sbonelosth <31245826+sbonelosth@users.noreply.github.com>
2025-09-07 05:48:28 +0200

    cli scripts patch

--- cli.js
+++ cli.js
@@ -208,7 +208,7 @@ config.User = User;
     const PORT = process.env.PORT || 4001;
     app.listen(PORT, () => console.log(\`🚀 Server running on http://localhost:\${PORT}\`));
   } catch (err) {
-    console.error("❌ Mongo connection failed:", err.message);
+    console.error("❌ MERN Access connection failed:", err.message);
     process.exit(1);
   }
 })();
